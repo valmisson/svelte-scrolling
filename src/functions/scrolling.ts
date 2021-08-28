367c72a68c7e437f2edc5b851aa7e74cb50e1a03
Valmisson Grizorte <missongrizorte@gmail.com>
2021-08-28 17:28:11 -0300

    feat: add function `scrollElement` (#2)

--- src/functions/scrolling.ts
+++ src/functions/scrolling.ts
@@ -1,4 +1,8 @@
+import { get } from 'svelte/store'
+import { elements } from '../store'
 import { getGlobalOptions } from '../internal/globalOptions'
+import { getElement, getPosition, sanitize } from '../shared/utils'
+import type { ScrollToOptions } from '../types/options'
 import scrolling from '../shared/scrolling'
 
 const globalOptions = getGlobalOptions()
@@ -46,4 +50,45 @@ export const scrollBottom = (
   )
 
   scrolling(end, opts)
+}
+
+/**
+ * Scroll to element
+ *
+ * @param options - The element reference or global options
+ */
+
+export const scrollElement = (
+  options: string | ScrollToOptions
+): void => {
+  if (!options) {
+    throw new Error('scrollElement require a options')
+  }
+
+  let opts: ScrollToOptions = {
+    ref: '',
+    ...globalOptions
+  }
+
+  typeof options === 'string'
+    ? opts.ref = options
+    : opts = Object.assign(options, opts)
+
+  const ref = sanitize(opts.ref)
+
+  if (!ref) {
+    throw new Error('scrollElement require a reference')
+  }
+
+  const { offset, duration, easing } = opts
+
+  const elementsList = get(elements)
+
+  const element = getElement(elementsList, ref)
+
+  if (!element) {
+    throw new Error(`Element reference '${ref}' not found`)
+  }
+
+  scrolling(getPosition(element), { duration, offset, easing })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as scrollTo } from './actions/ScrollTo'
 export { default as scrollRef } from './actions/ScrollRef'
-export { scrollTop, scrollBottom } from './functions/scrolling'
 export { default as setGlobalOptions } from './internal/globalOptions'
+export { scrollTop, scrollBottom, scrollElement } from './functions/scrolling'
